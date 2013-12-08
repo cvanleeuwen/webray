1204d237f1f0e3d8330aa737f35a10a81b1e67aa
Chris van Leeuwen <chrvanleeuwen@deloitte.nl>
2013-12-08 22:05:47 +0100

    added links because my mind resets when i sleep

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,7 +2,11 @@
 
 angular.module('webpxr')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
+    // now add http://www.ng-newsletter.com/posts/d3-on-angular.html into this
+	// see https://gist.github.com/auser/6506865#file-d3-example1-js
+	
+	
+	$scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
